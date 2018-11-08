32f0ce76e5a8dcfb81f521825e702122d64dd296
Danijel Temraz <danijel.temraz@infobip.com>
2018-11-08 16:03:31 +0100

    extended ascii

--- src/main/java/utilities/text/LanguageUtils.java
+++ src/main/java/utilities/text/LanguageUtils.java
@@ -1,24 +1,60 @@
 package utilities.text;
 
+import java.text.Normalizer;
+import java.util.regex.Pattern;
+
 /**
  * @author dtemraz
  */
 public class LanguageUtils {
 
     private static final int ASCII_RADIX = 128;
+    private static final int EXTENDED_ASCII_RADIX = 256;
+
     private static final UnicodeBlock MYANMAR_BASE = new UnicodeBlock(0x1000, 0x109F);
     private static final UnicodeBlock MYANMAR_EXTENDED_A = new UnicodeBlock(0xAA60, 0xAA7F);
     private static final UnicodeBlock MYANMAR_EXTENDED_B = new UnicodeBlock(0xA9E0, 0xA9FF);
 
+    private static final Pattern ASCII = Pattern.compile("[^\\p{ASCII}]"); // replace everything that is not ascii
+
     /**
-     * Returns true if <strong>all</strong> characters in text are in ASCII alphabet, false otherwise.
+     * Strips accents from the text using canonical decomposition(NFD) and replaces them with their ASCII counterparts.
+     * All non ascii characters will be removed from the <em>text</em>.
+     *
+     * @param text from which to strip accents
+     * @return text stripped of accents and with only ascii characters
+     */
+    public static String stripAccents(String text) {
+        if (text == null) {
+            return null;
+        }
+        return ASCII.matcher(Normalizer.normalize(text, Normalizer.Form.NFD)).replaceAll("");
+    }
+
+    /**
+     * Returns <em>true</em> if <strong>all</strong> characters in text are in ASCII alphabet, <em>false</em> otherwise.
      *
      * @param text to check if in ASCII
-     * @return true if all characters in text are in ASCII alphabet, false otherwise
+     * @return <em>true</em> if all characters in text are in ASCII alphabet, <em>false</em> otherwise
      */
     public static boolean inAsciiAlphabet(String text) {
+        return inAsciiAlphabet(text, ASCII_RADIX);
+    }
+
+    /**
+     * Returns <em>true</em> if <strong>all</strong> characters in text are in EXTENDED ASCII alphabet, <em>false</em> otherwise.
+     *
+     * @param text to check if in ASCII
+     * @return <em>true</em> if all characters in text are in ASCII alphabet, <em>false</em> otherwise
+     */
+    public static boolean inExtendedAsciiAlphabet(String text) {
+        return inAsciiAlphabet(text, EXTENDED_ASCII_RADIX);
+    }
+
+    // checks if entire text is in ascii, or extended ascii alphabet, depending on the radix
+    private static boolean inAsciiAlphabet(String text, int radix) {
         for (char c : text.toCharArray()) {
-            if (c >= ASCII_RADIX) {
+            if (c >= radix) {
                 return false;
             }
         }
